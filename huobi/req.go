e2c0a22c6df91bc7d4614b36f21992c707520ae7
Esiarp <37574105+Praise008@users.noreply.github.com>
2018-05-17 17:43:25 +0800

    Update req.go

--- huobi/req.go
+++ huobi/req.go
@@ -11,6 +11,13 @@ import (
     "net/url"
     "huobi/conf"
 )
+type DataStruct struct {
+    Open   float64 `json:"open"`
+}
+type ResStruct struct {
+    Data     []DataStruct `json:"data"`
+}
+
 func main()  {
     dialer := websocket.Dialer{Proxy:func(*http.Request) (*url.URL, error) {
         return url.Parse("http://127.0.0.1:1080")
@@ -20,18 +27,9 @@ func main()  {
     if err != nil {
     	// handle error
     }
-    reqMap := new(common.SubStruct)//创建结构体指针
-    reqMap.Id = strconv.Itoa(time.Now().Nanosecond())
-    reqMap.Sub = conf.LtcTopic.KLineTopicDesc
-    reqBytes , err := json.Marshal(reqMap)
-    //fmt.Println(string(reqBytes))
-    errr := ws.WriteMessage(websocket.TextMessage,reqBytes)
-    if errr != nil{
-        fmt.Errorf(err.Error())
-    }
-    
+
     for {        //因为是websocket,所以是不停的
-        if _,p,err := ws.ReadMessage();err == nil {
+        if _,p,err := ws.ReadMessage();err == nil {  //读取信息,这里是火币的规则
             res := common.UnGzip(p)            //解压数据
             //fmt.Println(string(res))           //输出字符串
             resMap := common.JsonDecodeByte(res)   //JSON解码
@@ -39,16 +37,33 @@ func main()  {
                 pingMap := make(map[string]interface{})
                 pingMap["pong"] = v            //发送pong包,完成ping,pong
                 pingParams := common.JsonEncodeMapToByte(pingMap)  //转成JSON
-                err := ws.WriteMessage(websocket.TextMessage, pingParams)
-                if err != nil{
-                    fmt.Errorf(err.Error())
+                if err := ws.WriteMessage(websocket.TextMessage, pingParams); err == nil {  //发送消息,TextMessage是整型,整数常量来标识两种数据消息类型
+                    reqMap := new(common.ReqStruct)//创建结构体指针
+                    reqMap.Id = strconv.Itoa(time.Now().Nanosecond())
+                    reqMap.Req = conf.LtcTopic.KLineTopicDesc
+                    reqBytes , err := json.Marshal(reqMap)
+                    fmt.Println(string(reqBytes))
+                    if err!=nil {
+                        continue
+                    }
+                    if err := ws.WriteMessage(websocket.TextMessage,reqBytes); err == nil {  //发送ID REQ
+                    }else{
+                        fmt.Errorf("send req response error %s",err.Error())
+                    }
+                }else{
+                    fmt.Errorf("huobi server ping client error %s",err.Error())
+                    continue
                 }
             }
-            if  vv, okk := resMap["tick"];okk  {
-                bch_values := vv.(map[string]interface{})         
-                fmt.Println(bch_values["open"])
+            if  _, ok := resMap["rep"];ok  {//获取回应的API数据
+                var resStruct ResStruct
+                json.Unmarshal(res,&resStruct)
+                //resStruct.Status
+                //fmt.Printf("%+v",resStruct.Data)//输出内容
+                for _,v:=range resStruct.Data{
+                    fmt.Println(v.Open)
+                }
             }
-            
         }
     }
 }
